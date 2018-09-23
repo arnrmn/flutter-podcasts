37728c252282778cf10a36b494a8f79a69ad05e0
Arnoldas Armonas <arnrmn@telesoftas.com>
2018-09-23 12:35:19 +0300

    Setup UI of list screen

--- lib/new_list.dart
+++ lib/new_list.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:podcast/entity/article.dart';
 import 'package:podcast/network/news_network_client.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class NewsListScreen extends StatelessWidget {
   @override
@@ -13,9 +14,11 @@ class NewsListScreen extends StatelessWidget {
         child: FutureBuilder<List<Article>>(
           builder: (_, snapshot) {
             if (snapshot.hasData) {
-              return Column(children: _buildArticles(snapshot.data));
+              return ListView(children: _buildArticles(snapshot.data));
             } else if (snapshot.hasError) {
               return Text("${snapshot.error}");
+            } else {
+              return CircularProgressIndicator();
             }
           },
           future: _fetchData(),
@@ -25,11 +28,35 @@ class NewsListScreen extends StatelessWidget {
   }
 
   List<Widget> _buildArticles(List<Article> articles) {
-    return articles.map((article) => Text(article.title)).toList();
+    return articles.map((article) => _buildArticleCard(article)).toList();
   }
 
   Future<List<Article>> _fetchData() {
-    NewsNetworkClient client = NewsNetworkClient();
-    return client.loadArticles();
+    return NewsNetworkClient().loadArticles();
+  }
+
+  Widget _buildArticleCard(Article article) {
+    return Card(
+      elevation: 0.0,
+      margin: EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+      child: InkWell(
+        onTap: () {},
+        child: Row(
+          children: <Widget>[
+            Container(
+              width: 160.0,
+              height: 90.0,
+              child:
+                  Image(image: CachedNetworkImageProvider(article.urlToImage)),
+            ),
+            Expanded(
+                child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(article.title),
+            )),
+          ],
+        ),
+      ),
+    );
   }
 }
