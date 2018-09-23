2106dbad9fb40a49dbfe83e113a6a4fbdfdf4c8c
Arnoldas Armonas <arnrmn@telesoftas.com>
2018-09-23 13:02:36 +0300

    Open details screen when clicked on card

--- lib/new_list.dart
+++ lib/new_list.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:podcast/entity/article.dart';
 import 'package:podcast/network/news_network_client.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:podcast/news_details.dart';
 
 class NewsListScreen extends StatelessWidget {
   @override
@@ -14,7 +15,7 @@ class NewsListScreen extends StatelessWidget {
         child: FutureBuilder<List<Article>>(
           builder: (_, snapshot) {
             if (snapshot.hasData) {
-              return ListView(children: _buildArticles(snapshot.data));
+              return ListView(children: _buildArticles(snapshot.data, context));
             } else if (snapshot.hasError) {
               return Text("${snapshot.error}");
             } else {
@@ -27,20 +28,25 @@ class NewsListScreen extends StatelessWidget {
     );
   }
 
-  List<Widget> _buildArticles(List<Article> articles) {
-    return articles.map((article) => _buildArticleCard(article)).toList();
+  List<Widget> _buildArticles(List<Article> articles, BuildContext context) {
+    return articles
+        .map((article) => _buildArticleCard(article, context))
+        .toList();
   }
 
   Future<List<Article>> _fetchData() {
     return NewsNetworkClient().loadArticles();
   }
 
-  Widget _buildArticleCard(Article article) {
+  Widget _buildArticleCard(Article article, BuildContext context) {
     return Card(
       elevation: 0.0,
       margin: EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => NewsDetailsScreen()));
+        },
         child: Row(
           children: <Widget>[
             Container(
