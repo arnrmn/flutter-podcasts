597dbbf1697ef829732c3176aad9cbd7631be58e
Arnoldas Armonas <arnrmn@telesoftas.com>
2018-09-23 16:27:17 +0300

    Implement basic youtube request

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:podcast/new_list.dart';
+import 'package:podcast/podcasts_list.dart';
 
 class PodcastApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'JRE Podcast',
-      home: NewsListScreen(),
+      home: PodcastsListScreen(),
     );
   }
 }
